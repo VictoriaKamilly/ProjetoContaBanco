278fff0449da4078316c51cce3d6850b483982c8
Victoria Kamilly <viictoriakamillydesouza@gmail.com>
2025-11-21 17:59:40 -0300

    Criando a logica para receber os valores

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US); //
+
+        System.out.print("Informe o número: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // limpa o ENTER pendente
+
+        System.out.print("Informe o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Informe o nome do titular: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Informe o saldo que deseja depositar: ");
+        String saldoStr = scanner.next().replace(",", ".");
+        double saldo = Double.parseDouble(saldoStr);
+
+        System.out.println("Olá " + nomeCliente
+                + "! Obrigado por criar uma conta em nosso banco. Sua agência é "
+                + agencia + ", conta " + numero
+                + " e seu saldo " + saldo + " já está disponível para saque!");
+
+        scanner.close();
     }
 }
